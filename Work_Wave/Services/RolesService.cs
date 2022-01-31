79bd2d428630d1db24003cdf7bd57a1c7ef4bdda
M-Peach <mikepeachman@live.com>
2022-01-31 17:59:10 -0500

    Code Cleanup

--- Work_Wave/Services/RolesService.cs
+++ Work_Wave/Services/RolesService.cs
@@ -26,52 +26,9 @@ namespace Work_Wave.Services
             return result;
         }
 
-        public async Task<string> GetRoleNameByIdAsync(string roleId)
-        {
-            IdentityRole role = _context.Roles.Find(roleId);
-            string result = await _roleManager.GetRoleNameAsync(role);
-            return result;
-        }
-
-        public async Task<string> GetRoleIdByNameAsync(string role)
-        {
-            IdentityRole id = _context.Roles.Find(role);
-            string result = await _roleManager.GetRoleIdAsync(id);
-            return result;
-        }
-
         public async Task<IEnumerable<string>> GetUserRolesAsync(WaveUser user)
         {
             IEnumerable<string> result = await _userManager.GetRolesAsync(user);
-            return result;
-        }
-
-        public async Task<List<WaveUser>> GetUsersInRoleAsync(string roleName)
-        {
-            List<WaveUser> users = (await _userManager.GetUsersInRoleAsync(roleName)).ToList();
-            List<WaveUser> result = users.ToList();
-            return result;
-        }
-
-        public async Task<List<WaveUser>> GetUsersNotInRoleAsync(string roleName)
-        {
-            List<string> userIds = (await _userManager.GetUsersInRoleAsync(roleName)).Select(u => u.Id).ToList();
-            List<WaveUser> roleUsers = _context.Users.Where(u => !userIds.Contains(u.Id)).ToList();
-
-            List<WaveUser> result = roleUsers.ToList();
-            return result;
-
-        }
-
-        public async Task<bool> IsUserInRoleAsync(WaveUser user, string roleName)
-        {
-            bool result = await _userManager.IsInRoleAsync(user, roleName);
-            return result;
-        }
-
-        public async Task<bool> RemoveUserFromRoleAsync(WaveUser user, string roleName)
-        {
-            bool result = (await _userManager.RemoveFromRoleAsync(user, roleName)).Succeeded;
             return result;
         }
 
